06f591dd567de2cf98cf3fb766f427dd3f84e5ad
Manimaran N <manimaran@dexlock.com>
2020-06-04 23:19:32 +0530

    Updates

---
+++ precache-manifest.839f0788e0e49bde8de075ea1a6b1b94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978047cc400d0028b590cd15547bde16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfb83717b10acfbfcf4d",
+    "url": "/static/css/main.37ec4897.chunk.css"
+  },
+  {
+    "revision": "492636cc34816fea4855",
+    "url": "/static/js/2.9c9ac907.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c9ac907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb83717b10acfbfcf4d",
+    "url": "/static/js/main.35566697.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "0040798942c92fc885a3c9437204871f",
+    "url": "/static/media/header-image.00407989.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
